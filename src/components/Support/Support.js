565c00014771d7cb95445339fe7363ce56f93241
Adam Hay <heyadam66@gmail.com>
2019-07-20 17:16:09 -0500

    Added radio buttons to support component

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -11,7 +11,17 @@ class Support extends Component {
     render() {
         return(
             <>
-                <p>How well are you being supported?</p>
+                <p>How well are you being supported? 1 being not at all; 5 being you feel extremely supported</p>
+                <input type="radio" name="feedback" value="1" />1 
+                <br/>
+                <input type="radio" name="feedback" value="2" />2
+                <br/>
+                <input type="radio" name="feedback" value="3" />3
+                <br/>
+                <input type="radio" name="feedback" value="4" />4
+                <br/>
+                <input type="radio" name="feedback" value="5" />5 
+                <br/>
                 <button onClick={this.handleSubmit}>Next</button>
             </>
         )
