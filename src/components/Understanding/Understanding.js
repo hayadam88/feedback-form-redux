89b86e3373dce6325c390a739b94c18196c1172f
Adam Hay <heyadam66@gmail.com>
2019-07-20 17:14:32 -0500

    Added radio buttons to understanding component

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -11,7 +11,17 @@ class Understanding extends Component {
     render() {
         return(
             <>
-                <p>How well are you understanding today's material?</p>
+                <p>How well are you understanding today's material? 1 being totally lost; 5 being you could teach it yourself.</p>
+                <input type="radio" name="feedback" value="1" />1 
+                <br/>
+                <input type="radio" name="feedback" value="2" />2
+                <br/>
+                <input type="radio" name="feedback" value="3" />3
+                <br/>
+                <input type="radio" name="feedback" value="4" />4
+                <br/>
+                <input type="radio" name="feedback" value="5" />5 
+                <br/>
                 <button onClick={this.handleSubmit}>Next</button>
             </>
         )
