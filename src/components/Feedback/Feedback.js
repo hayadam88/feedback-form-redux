ddfd617a5873b2ae56c7f69966f9585bf32f0819
Adam Hay <heyadam66@gmail.com>
2019-07-21 14:52:05 -0500

    Changed a few things on feedback component

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -6,26 +6,19 @@ import Axios from 'axios';
 class Feedback extends Component {
 
     state = {
-        feeling: 0,
-        understanding: 0,
-        support: 0,
-        comments: ''     
-    }
-
-
-    handleFinalSubmit = () => {
-        this.setState({
             feeling: this.props.reduxStore.feelingsReducer.feeling,
             understanding: this.props.reduxStore.understandingReducer.understanding,
             support: this.props.reduxStore.supportReducer.support,
-            comments: this.props.reduxStore.commentReducer.comments 
-        })
+            comments: this.props.reduxStore.commentReducer.comments     
+            }
+
+
+    handleFinalSubmit = () => {
         console.log('Clicked submit feedback', this.state);
         document.getElementById("submitBtn").disabled = true;
          Axios.post('/feedback', this.state)
          .then(response => {
              console.log(response);
-             this.props.history.push('/finish');
          }).catch(err => {
              console.log(err);
          })
