367b7b2566eee7e0447b954dce06db070dc358ca
Adam Hay <heyadam66@gmail.com>
2019-07-21 14:40:55 -0500

    POST working, just not getting the right values

--- server/server.js
+++ server/server.js
@@ -18,8 +18,8 @@ app.post('/feedback', (req, res) => {
 
     let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
                    VALUES ($1, $2, $3, $4);`;
-    pool.query(queryText, newFeedback.feeling, newFeedback.understanding, newFeedback.support,
-    newFeedback.comments)
+    pool.query(queryText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support,
+    newFeedback.comments])
         .then(result => {
             res.sendStatus(201);
         })
